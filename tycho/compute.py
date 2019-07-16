916884bf67283db77f057ae307ef57505c65203a
muralikarthikk <muralikarthik.k123@gmail.com>
2019-07-16 15:16:51 +0000

    updated compute.py

--- tycho/compute.py
+++ tycho/compute.py
@@ -191,7 +191,8 @@ class KubernetesCompute(Compute):
         try: 
             name = "pv-for-" + name
             api_response = self.api.delete_persistent_volume(
-                name=name)
+                name=name,
+                body=k8s_client.V1DeleteOptions())
             print(api_response)
         except ApiException as e:
             print("Exception when calling CoreV1Api->delete_persistent_volume: %s\n" % e)
