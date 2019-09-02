5260c7238db0319f5bcdfd9c471797cbd315d74a
muralikarthikk <41588376+muralikarthikk@users.noreply.github.com>
2019-09-02 00:43:39 -0400

    Update kube.py

--- tycho/kube.py
+++ tycho/kube.py
@@ -126,16 +126,17 @@ class KubernetesCompute(Compute):
                         api_response = self.rbac_api.create_cluster_role(body=cluster_role_manifest)
             except Exception as e:
                 logger.error(f"cannot create cluster role: {e}")
-                traceback.print_exc (file=open("logs.txt", "a"))
+                traceback.print_exc (file=open("clusterrolelogs.txt", "a"))
             
-            logger.debug("creating cluster role binding")
-            cluster_role_binding_manifests = system.render(template=f"{system.system_name}/clusterrolebinding.yaml")
-            for cluster_role_binding_manifest in cluster_role_binding_manifests:
-                logger.debug(f"applying cluster role binding: {cluster_role_binding_manifest}")
-                api_response = self.rbac_api.create_cluster_role_binding(body=cluster_role_binding_manifest)
-            #except Exception as e:
-            #    logger.error(f"cannot create cluster role binding: {e}")
-            #    traceback.print_exc (e)
+            try:
+                logger.debug("creating cluster role binding")
+                cluster_role_binding_manifests = system.render(template=f"{system.system_name}/clusterrolebinding.yaml")
+                for cluster_role_binding_manifest in cluster_role_binding_manifests:
+                    logger.debug(f"applying cluster role binding: {cluster_role_binding_manifest}")
+                    api_response = self.rbac_api.create_cluster_role_binding(body=cluster_role_binding_manifest)
+            except Exception as e:
+                logger.error(f"cannot create cluster role binding: {e}")
+                traceback.print_exc (file=open("clusterrolebindinglogs.txt", "a"))
             
             result = {
                 'name'       : system.name,
