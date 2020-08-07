1bd5f400b5eaf25ff779c4df367546ad5d040479
muralikarthikk <muralikarthik.k123@gmail.com>
2020-08-06 23:24:12 -0400

    unit tests for volume class in model.py

--- tycho/test/test_model_volume.py
+++ tycho/test/test_model_volume.py
@@ -2,40 +2,7 @@ import pytest
 import sys
 from tycho.model import Volumes
 
-@pytest.fixture(params=["data1", "data2"])
-def volume_model_data(request):
-    if request.param is "data1":
-        data = [{
-            "name": "nginx",
-            "image": "sample/image:v1",
-            "command": "some entrypoint",
-            "env": [],
-            "limits": [],
-            "requests": {},
-            "ports": [],
-            "expose": [],
-            "depends_on": [],
-            "volumes": ["pvc://nfsrods/rods:/home/rods", "pvc://cloud-top:/home/shared"]
-        }]
-        return data
-    if request.param is "data2":
-        data = [
-            {'container_name': 'nginx',
-             'pvc_name': 'nfsrods',
-             'volume_name': 'nfsrods',
-             'path': '/home/rods',
-             'subpath': 'rods'
-             },
-            {'container_name': 'nginx',
-             'pvc_name': 'cloud-top',
-             'volume_name': 'cloud-top',
-             'path': '/home/shared',
-             'subpath': ''
-             }
-        ]
-        return data
-
-
+# Sample data from conftest.py
 @pytest.mark.parametrize('volume_model_data', ['data1'], indirect=True)
 def test_model_volume(volume_model_data):
     id_num = "333333333"
